69a0ec8d37a5330c4f8cf32be64b3f2f382beaca
Yina Zhu <yinagit@gmail.com>
2018-04-16 21:58:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, pledges.amount
+"SELECT users.name, SUM(pledges.amount)
   FROM users
     JOIN pledges
       ON users.id = pledges.users_id
