9b9253440c670354f4bb3247bd9dffef0bbb263a
Yina Zhu <yinagit@gmail.com>
2018-04-16 22:03:46 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   FROM users
     JOIN pledges
       ON users.id = pledges.users_id
-        GROUP BY = users.name;"
+        GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
