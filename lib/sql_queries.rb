c66b931070ace97ae73a5ced9dc35c5326158ddc
Yina Zhu <yinagit@gmail.com>
2018-04-16 22:57:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,12 +41,12 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledges.amount)
+"SELECT projects.category, pledges.amount
   FROM projects
   JOIN pledges
   ON projects.category = pledges.user_id
   GROUP BY LIKE "music";
-  
+
   "
 end
 
