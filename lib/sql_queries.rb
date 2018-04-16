51894a444924e3b0a48267df306c2d5efb869ca6
Yina Zhu <yinagit@gmail.com>
2018-04-16 22:49:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,8 +35,9 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT users.name, SUM(pledges.amount)
   FROM users
   JOIN pledges
-  ON user_id = pledges.user_id
-  GROUP BY pledges.amount DESC;"
+  ON users.id = pledges.user_id
+  GROUP BY users.name
+  ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
